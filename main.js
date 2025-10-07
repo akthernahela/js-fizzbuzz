7b1357b7ec13cec961bb2d3ebcb1023c807aa65c
akthernahela <nahela00@gmail.com>
2025-10-07 13:51:50 +0200

    Change the Fizz and Buzz position, because it wasn't working

--- main.js
+++ main.js
@@ -15,9 +15,13 @@ proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro program
 così come lo faremmo "a mano" 
 */
 
-for ( let i = 1; i < 100; i++){
-    if (i % 5 === 0 ){
-        console.log('Fizz')
-    } else if (i % 3 === 0){
-        console.log('Buzz')
+for (let i = 1; i < 100; i++) {
+
+    if (i % 15 === 0) {
+        console.log('FizzBuzz');
+    } else if (i % 5 === 0) {
+        console.log('Fizz');
+    } else if (i % 3 === 0) {
+        console.log('Buzz');
     }
+}
